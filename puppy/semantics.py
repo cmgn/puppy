ff95f0a417b8c3119f860acea964ed4c7883ef07
cmgn <conormcgovern25@gmail.com>
2018-06-07 12:14:43 +0100

    added space to error msg

--- puppy/semantics.py
+++ puppy/semantics.py
@@ -7,7 +7,7 @@ def typechecked(valid_types, display_name):
             if type(arg) in valid_types:
                 return func(arg)
             raise ValueError(
-                "Function '{}' expected the type of the argument given to be"
+                "Function '{}' expected the type of the argument given to be "
                 "one of {}, but got an argument of type '{}'.".format(
                     display_name,
                     [_type.__name__ for _type in valid_types],
